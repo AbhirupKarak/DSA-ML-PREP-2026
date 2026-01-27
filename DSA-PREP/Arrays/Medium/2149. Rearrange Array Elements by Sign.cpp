a01b519b5f877300c8994fa2e00862ce3c552928
Abhirup Karak <83585973+AbhirupKarak@users.noreply.github.com>
2026-01-27 22:04:37 +0530

    Add solution for rearranging array elements by sign

---
+++ DSA-PREP/Arrays/Medium/2149. Rearrange Array Elements by Sign.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        int countPos = 0;
+        int countNeg = 1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] >= 0){
+                ans[countPos] = nums[i];
+                countPos += 2;
+            }
+            else{
+                ans[countNeg] = nums[i];
+                countNeg+=2;
+            }
+        }
+        return ans;
+    }
+};
