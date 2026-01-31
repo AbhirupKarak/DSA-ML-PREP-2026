d87b5fcdc6ee25393ba3d37a24042329ceddc3ae
Abhirup Karak <83585973+AbhirupKarak@users.noreply.github.com>
2026-01-31 23:45:09 +0530

    Create 73. Set Matrix Zeroes.cpp

---
+++ DSA-PREP/Arrays/Medium/73. Set Matrix Zeroes.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        bool firstRowZero = false;
+        bool firstColZero = false;
+
+        // Check first row
+        for (int j = 0; j < n; j++) {
+            if (matrix[0][j] == 0)
+                firstRowZero = true;
+        }
+
+        // Check first column
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == 0)
+                firstColZero = true;
+        }
+
+        // Mark rows and columns
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Zero marked rows
+        for (int i = 1; i < m; i++) {
+            if (matrix[i][0] == 0) {
+                for (int j = 1; j < n; j++)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        // Zero marked columns
+        for (int j = 1; j < n; j++) {
+            if (matrix[0][j] == 0) {
+                for (int i = 1; i < m; i++)
+                    matrix[i][j] = 0;
+            }
+        }
+
+        // Zero first row
+        if (firstRowZero) {
+            for (int j = 0; j < n; j++)
+                matrix[0][j] = 0;
+        }
+
+        // Zero first column
+        if (firstColZero) {
+            for (int i = 0; i < m; i++)
+                matrix[i][0] = 0;
+        }
+    }
+};
